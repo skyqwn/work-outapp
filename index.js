634ba95bbc87ae440d018d867508ed761ba706d8
kimba <skyqwn@gmail.com>
2022-08-04 11:00:32 +0900

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const height = document.querySelector(".height");
+const weight = document.querySelector(".weight");
+const startBtn = document.querySelector(".start-btn");
+const infoForm = document.querySelector("#info-form");
+const infoSave = document.querySelector(".info-svae");
+
+let myInfo = [];
